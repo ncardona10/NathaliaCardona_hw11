308c6e716548f4bb3c7aed647466726591522731
Nathalia <n.cardonac@uniandes.edu.co>
2018-04-18 15:24:05 -0500

    sistema gravitatorio unidimensional

---
+++ NC_graph.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#se importan los datos
+data = np.loadtxt("tray.txt")
+#se guardan los valores de x,v, t en array
+x = data[:,0]
+v = data[:,1]
+t = data[:,2]
+#imagen 1: posicion en funcion del tiempo
+plt.plot(x,t,label="Posicion")
+plt.title("x Vs t")
+plt.legend()
+plt.xlabel("Tiempo ")
+plt.ylabel("Posicion")
+plt.sevefig("pos.png")
+plt.close()
+#imagen 2: velocidad en funcion del tiempo
+plt.plot(v,t,label="Velocidad")
+plt.title("v Vs t")
+plt.legend()
+plt.xlabel("Tiempo ")
+plt.ylabel("Velocidad")
+plt.sevefig("vel.png")
+plt.close()
+#imagen 3: velocidad en funcion de la posicion
+plt.plot(x,v,label="Fase")
+plt.title("v Vs x")
+plt.legend()
+plt.xlabel("Posicion")
+plt.ylabel("Velocidad")
+plt.sevefig("phase.png")
+plt.close()
